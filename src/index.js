2af8e9a7ff7b08f05227a688980146f5b9758fe6
Vincent Garcia <24626196+Saitama2016@users.noreply.github.com>
2018-08-16 10:26:51 -0700

    Begin Chilli-Bin Layout

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import './index.css';
+
+ReactDOM.render(
+    <Provider >
+    </Provider>,
+    document.getElementById('root')
+)
